9fefff4763be82b4af753b9fb8e78eed0b744a4a
ksdkaveesha <149872713+ksdkaveesha@users.noreply.github.com>
2025-01-15 00:08:40 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -9,21 +9,27 @@ const Login = () => {
     };
 
     return (
-        <div className="h-screen flex justify-end items-center bg-[url('images/Login.jpg')] bg-cover bg-no-repeat bg-left">
-            <div className='md:h-3/4 md:w-1/3 bg-white flex justify-center items-center mr-20'>
-                <form className='w-3/4' action={handleLogin}>
-                    <div>
+        <div className="flex justify-center items-center">
+            <div className='md:w-2/3 flex justify-center items-center'>
+                <img src='images/Login.jpg' className='w-3/4'></img>
+            </div>
+            <div className='md:w-1/3 flex justify-center items-center h-full'>
+                <form className='' action=''>
+                    <div className='bg-slate-300 p-12 rounded-xl'>
                         <h1 className='font-bold text-5xl mb-8 text-gray-600'>Login</h1>
-                        <div className='flex flex-col mb-10'>
+                        <div className='flex flex-col mb-10 bg-transparent'>
                             <labal>Username</labal>
-                            <input type="text" className='border-b-2 border-black focus:outline-none w-full'></input>
+                            <input type="text" className='border-b-2 border-black focus:outline-none w-full bg-transparent'></input>
                         </div>
                         <div className='flex flex-col mb-10'>
                             <labal>Password</labal>
-                            <input type="password" className='border-b-2 border-black focus:outline-none w-full'></input>
+                            <input type="password" className='border-b-2 border-black focus:outline-none w-full bg-transparent'></input>
                         </div>
                         <div className='mb-10 flex justify-center items-center'>
                             <button type="submit" onClick={handleLogin} className='w-1/2 p-2 rounded-full bg-gray-600'>Login</button>
+                        </div>
+                        <div className='flex justify-center items-center mb-10'>
+                            <labal>If you don't have an account? <a href="#"><u><b>Sign Up</b></u></a></labal>
                         </div>
                     </div> 
                 </form>               
